fff6c77bac130579721e62bbd9611889132ccd32
AK47 <huynamyb@gmail.com>
2018-12-14 15:53:16 +0700

    Laravel Email Verification

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('url', 'KamalController@index')->name('url');
 
-Auth::routes(['verify'=>true]);
+Auth::routes(['verified'=>true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
@@ -27,3 +24,11 @@ Route::get('login', 'Auth\Admin\LoginController@login')->name('admin.auth.login'
 Route::post('login', 'Auth\Admin\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
 Route::post('logout', 'Auth\Admin\LoginController@logout')->name('admin.auth.logout');
 });
+
+//Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+
+
